0e948c3e493c206336dd44f47f2833e7f86b44ed
RETR0NACH <ichaconaedo@gmail.com>
2025-11-18 19:29:24 -0300

    UserRepository new

---
+++ src/main/java/com/example/basedato/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.basedato.repository;
+
+import com.example.basedato.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UserRepository {
+
+}
