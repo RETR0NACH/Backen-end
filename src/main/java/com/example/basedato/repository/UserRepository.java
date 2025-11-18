3e161dec8a444d393b53a7fcacce56f33a603158
RETR0NACH <ichaconaedo@gmail.com>
2025-11-18 19:31:45 -0300

    Método extra para buscar por email (usado en el login)

--- src/main/java/com/example/basedato/repository/UserRepository.java
+++ src/main/java/com/example/basedato/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.basedato.repository;
 import com.example.basedato.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-public interface UserRepository {
+public interface UserRepository  extends JpaRepository<User, Long>{
+    Optional<User> findByEmail(String email);
 
 }
