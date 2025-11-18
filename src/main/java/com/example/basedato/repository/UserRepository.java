e6d061927dec9e4530d5ff1b3a4bb8556a8c3c51
MauroAlmonacid <canepheler@gmail.com>
2025-11-18 20:05:00 -0300

    s

--- src/main/java/com/example/basedato/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.basedato.repository;
-
-import com.example.basedato.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-public interface UserRepository {
-
-}
