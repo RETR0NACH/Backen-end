206dc5d458ba3d54bc88a1dcc6188bd3fbdaf49c
RETR0NACH <ichaconaedo@gmail.com>
2025-11-23 02:36:42 -0300

    creación de RegisterRequest

---
+++ src/main/java/com/example/basedato/auth/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.example.basedato.auth;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+}
