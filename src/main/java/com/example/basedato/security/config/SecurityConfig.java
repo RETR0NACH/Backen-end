6d04d7eda2a0fec01f620f02b37e469a57f4ba32
RETR0NACH <ichaconaedo@gmail.com>
2025-11-24 14:33:41 -0300

    /

--- src/main/java/com/example/basedato/security/config/SecurityConfig.java
+++ src/main/java/com/example/basedato/security/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("https://*.vercel.app", "http://localhost:*","https://andromeda-s-inn-shop-3eva.vercel.app/"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://*.vercel.app", "http://localhost:*","https://andromeda-s-inn-shop-3eva.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
